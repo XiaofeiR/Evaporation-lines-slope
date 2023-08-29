8cc7ab68f6afae3c9d7cad0d78a5d9cb7bb12ffd
Xiaofei Ren <143474491+XiaofeiR@users.noreply.github.com>
2023-08-29 17:15:16 +0800

    Update EL_slope.py

--- EL_slope.py
+++ EL_slope.py
@@ -8,7 +8,6 @@
 
 import numpy as np
 
-
 #-----------------------------------------------------------
 # Calculate α+ ： The calculation formula comes from Horita and Wesolowski(1994)
 # α+ is the liquid-vapor equilibrium isotopic fractionation
@@ -68,10 +67,10 @@ def epsilon_k(atom,n,method,h):
     n: 0.5~1 ; 0.5 for open water bodies; 1 for soil water
 
     method : 0 or 1
-        0 : Gibson(2008) GBC 这里存在一个问题就是C值 Haowen Liu （CATENA）中C值与原版相差10
+        0 : Gibson(2008) GBC
         1 ：Paolo Benettin (2018) HESS
 
-    h : relative humidity
+    h : relative humidity.  e.g. 0.65
 
     return : ‰
     """
@@ -103,6 +102,7 @@ def epsilon_k(atom,n,method,h):
 # ---------------------------------------------------------------
 #  Calculate δA  ： δA=(δP-ε+)/α+
 def delta_A(delta_P,epsilon,alpha):
+
     i = (delta_P-epsilon)/alpha
     return i
 #------------------------------------------------------------------
@@ -110,18 +110,14 @@ def delta_A(delta_P,epsilon,alpha):
 
 #-----------------------------------------------------------------
 # Calculate slope
-# slope = dX_H / dX_O
+
 #根据 论文 Recharge mechanisms of deep soil water revealed by water isotopes in deep loess deposits中的计算公式这个公式与 Haowen Liu (2022)中的公式相同 这里dX值仅仅为计算slope的氢或者氧
-
-
 
 def dX(atm,Pre,h,t,n,method):
     """
-    atm : 'H' or 'O'.   e.g. 'H'
     Pre : isotopic composition of precipitation.  e.g. [-38,-6]
-    h   : relative humidity.  e.g. 0.67
-    t   : temperature.  e.g. 30
-    n   : 0.5~1.  e.g. 0.5
+
+    Other parameters are the same as above
 
     """
     
@@ -163,15 +159,16 @@ def dX(atm,Pre,h,t,n,method):
         
 # dX('H',[-38,-6],0.75,20,0.75,1)
 
-
 def slope_line(Pre,h,t,n,method):
     """
-    计算 蒸发线的斜率 具体的公式的表述见dX
-    Pre： 降雨同位素 [H,O]  单位 ‰
-    h ： 相对湿度
-    t ： 温度 摄氏度℃
-    n ： 依据参考文献 开放水体取0.5  土壤水取 1
-    method ： 计算 epsilon_K方法， 主要参照epsilon_K的过程
+    slope = dX_H / dX_O
+
+    Pre : e.g. [-36,-6]
+    h   : e.g. 0.75
+    t   : e.g. 20
+    n   : e.g. 1
+    method :  e.g. 1
+
     """
     d1 = dX('H',Pre,h,t,n,method)
 
@@ -179,15 +176,3 @@ def slope_line(Pre,h,t,n,method):
 
     return d1/d2
 # slope_line([-38,-6],0.75,20,0.75,1)
-# -------------------------------------------------------------
-
-
-
-if __name__ == 'main':
-    print('Benettin示例Matlab代码一致均为')
-    print(slope_line([-38,-6],0.75,20,0.75,1))
-    # 与Benettin计算结果一致，可以用于后续计算
-
-
-# T = tk(10)
-# alpha_plus('H',T)
